91663d724fe6c6c3db0640f4f1f2fef249941973
farazyounus11 <40037013+farazyounus11@users.noreply.github.com>
2024-04-18 15:51:51 -0400

    sales1.py

--- sales1.py
+++ sales1.py
@@ -1,40 +1,25 @@
 import streamlit as st
 import pandas as pd
-import pydeck as pdk
 import streamlit_pandas as sp
-import os
-import re
 
-st.set_page_config(layout="wide")
-st.title("Sales Data Analysis")
-st.header('By Faraz Younus | M.S. Stats & Data Science', divider='gray')
-st.markdown("### Use the Sidebar to Filter Data")    
-
-@st.cache
-def load_data(file_path):
-    df = pd.read_csv(file_path)
+@st.cache_data
+def load_data():
+    df = pd.read_csv(file)
     return df
 
-files = os.listdir()  
-selected_file = st.selectbox("Select a file", files)
+file = "./data/titanic.csv"
+df = load_data()
+create_data = {"Name": "text",
+                "Sex": "multiselect",
+                "Embarked": "multiselect",
+                "Ticket": "text",
+                "Pclass": "multiselect"}
 
-df1 = load_data(selected_file)  
-
-
-
-create_data = {"Order ID": "text",
-                "Product": "multiselect",
-                "Purchase Address": "text",
-               "Order Date": "multiselect"
-               }
-
-all_widgets = sp.create_widgets(df1, create_data)
-
-df = sp.filter_df(df1, all_widgets)
+all_widgets = sp.create_widgets(df, create_data, ignore_columns=["PassengerId"])
+res = sp.filter_df(df, all_widgets)
 st.title("Streamlit AutoPandas")
 st.header("Original DataFrame")
-st.write(df1)
-
-st.header("Result DataFrame")
 st.write(df)
 
+st.header("Result DataFrame")
+st.write(res)
